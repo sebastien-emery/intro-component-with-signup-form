630bd9645dea718e4604b9bfc698dde4f6cf1b65
Seb <emery.sebastien@hotmail.fr>
2022-06-05 23:30:57 +0200

    début dynamisation input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import fields from './fields.config';
+
 import './style.scss';
 
 const Form = () => {
+    const [firstName, setFirstName] = useState('Salut');
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChangeFirstName = (e) => {
+        console.log('handleChangeFirstName');
+        setFirstName(e.target.value);
+    }
+
     return (
         <div className='container'>
-            <form className='container__form'>
-                <input type="text" placeholder='First Name' className='container__form__firstname' />
-                <input type="text" placeholder='Last Name' className='container__form__lastname' />
-                <input type="mail" placeholder='Email Address' className='form__mail' />
-                <input type="password" placeholder='Password' className='container__form__password' />
-                <button className='container__form__submit'>Claim your free trial</button>
+            <form className='container__form' onSubmit={handleSubmitForm}>
+                {
+                    fields.map((field) => (
+                        <input
+                            {...field}
+                        />
+                    ))
+                }
+                <button type="submit" className='container__form__submit'>Claim your free trial</button>
             </form>
             <p className='container__warning'>
                 By clicking the button, you are agreeing to our <span>Terms and Services</span>
--- src/components/InfoBox/index.js
+++ src/components/InfoBox/index.js
@@ -1,7 +1,6 @@
 import './style.scss';
 
 const InfoBox = () => {
-    console.log('InfoBox');
     return (
         <div className="infoBox">
             <span className='infoBox__bold'>Try it free 7 days</span> then $20/mo. thereafter
